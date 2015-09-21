7d755d276322603c3ef9375246a33ed8bb0f6a57
ryosukee <tmcit.miyazaki@gmail.com>
2015-09-21 20:32:55 +0900

    [add] nullの画像を追加

--- mapEditor/src/mapeditor/LayerPanel.java
+++ mapEditor/src/mapeditor/LayerPanel.java
@@ -10,38 +10,22 @@ public class LayerPanel extends JPanel {
     // マップの大きさ（単位：マス）
     private int row;
     private int col;
+    // レイヤー番号
+    private int layer_num;
     // マップチップパレット
     private PaletteDialog paletteDialog;
     private AutoTilePaletteDialog autoTileDialog;
     private static final int CHIP_SIZE = MainPanel.CHIP_SIZE;
    
-    public LayerPanel(int r, int c, PaletteDialog pt, AutoTilePaletteDialog at) {
-        // マップを初期化
-        initMap(16, 16);
+    public LayerPanel(int lnum, int r, int c, PaletteDialog pt, AutoTilePaletteDialog at) {
+        this.layer_num = lnum;
         this.paletteDialog = pt;
         this.autoTileDialog = at;
         this.setOpaque(false);
+        // マップを初期化
+        initMap(16, 16);
     }
     
-    public void setIdOnMap(int y, int x) {
-        System.out.println("Before MainPanel map[y][x] = " + map[y][x]);
-        // パレットから取得した番号をセット
-        if (x >= 0 && x < col && y >= 0 && y < row) {
-        	if (paletteDialog.lastSelected){
-        		map[y][x] = paletteDialog.getSelectedMapChipNo();
-        	} else if (autoTileDialog.lastSelected) {
-        		map[y][x] = autoTileDialog.getSelectedMapChipNo();
-        	} else {
-        		System.out.println("例外投げるべきだけどprintしちゃう！\n"
-        				+ "マップチップ選択フラグ(paletteかauto tile paletetか)がおかしい！！");;
-        	}
-        	// 周辺のautoTileを更新
-        	autoTileDialog.updateMapChipImage(y, x, map);
-        }
-        System.out.println("After  MainPanel map[y][x] = " + map[y][x]);
-
-    }
-
     /**
      * マップを初期化する
      * @param row 行数
@@ -54,7 +38,7 @@ public class LayerPanel extends JPanel {
 
         // パレットで選択されているマップチップの番号を取得
         // int mapChipNo = paletteDialog.getSelectedMapChipNo();
-        int mapChipNo = 41;
+        int mapChipNo = 2000;
         
         // そのマップチップでマップを埋めつくす
         for (int i = 0; i < r; i++) {
@@ -64,6 +48,27 @@ public class LayerPanel extends JPanel {
         }
     }
    
+    public void setIdOnMap(int y, int x) {
+    	System.out.println();
+        System.out.println("Current Layer:" + layer_num);
+        System.out.println("Before map["+y+"]["+x+"] = " + map[y][x]);
+        // パレットから取得した番号をセット
+        if (x >= 0 && x < col && y >= 0 && y < row) {
+        	if (paletteDialog.lastSelected){
+        		map[y][x] = paletteDialog.getSelectedMapChipNo();
+        	} else if (autoTileDialog.lastSelected) {
+        		map[y][x] = autoTileDialog.getSelectedMapChipNo();
+        	} else {
+        		System.out.println("例外投げるべきだけどprintしちゃう！\n"
+        				+ "マップチップ選択フラグ(paletteかauto tile paletetか)がおかしい！！");;
+        	}
+        	// 周辺のautoTileを更新
+        	autoTileDialog.updateMapChipImage(y, x, layer_num, map);
+        }
+        System.out.println("After map["+y+"]["+x+"] = " + map[y][x]);
+
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -72,7 +77,7 @@ public class LayerPanel extends JPanel {
             for (int j = 0; j < col; j++) {
             	int chip_id = map[i][j];
             	if (autoTileDialog.isAutoTile(chip_id)) {
-                    g.drawImage(autoTileDialog.getMapChipImage(i, j), j * CHIP_SIZE, i * CHIP_SIZE, null);
+                    g.drawImage(autoTileDialog.getMapChipImage(i, j, layer_num), j * CHIP_SIZE, i * CHIP_SIZE, null);
             	}else {
             		g.drawImage(paletteDialog.getMapChipImage(chip_id), j * CHIP_SIZE, i * CHIP_SIZE, null);
             	}
